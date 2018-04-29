8e38d0706c612df0c6e9ef1be8f051706c506bae
LarsonYong <jianbo1889@gmail.com>
2018-04-29 15:54:04 -0700

    add

--- react-redux-client/src/_components/Todo.jsx
+++ react-redux-client/src/_components/Todo.jsx
@@ -7,10 +7,14 @@ import { Table } from 'react-bootstrap';
 import Button from 'react-bootstrap/lib/Button';
 import { todoActions} from '../_actions';
 import { todoService } from '../_services';
+import TodoEditForm from './TodoEditForm';
+
 
 class Todo extends React.Component {
   constructor(props){
     super(props);
+    this.hideEditModal = this.hideEditModal.bind(this);
+    this.submitEditTodo = this.submitEditTodo.bind(this);
 
   }
 
@@ -18,8 +22,36 @@ class Todo extends React.Component {
     this.props.dispatch(todoActions.getAll());
   }
 
+  showEditModal(){
+
+  }
+
+  hideEditModal(){
+    this.props.mappedhideEditModal();
+  }
+
+  submitEditTodo(e){
+    e.preventDefault();
+    const editForm = document.getElementById('EditTodoForm');
+    if(editForm.todoText.value !== ""){
+      const data = new FormData();
+      data.append('id', editForm.id.value);
+      data.append('todoText', editForm.todoText.value);
+      data.append('todoDesc', editForm.todoDesc.value);
+      this.props.mappedEditTodo(data);
+    }
+    else{
+      return;
+    }
+  }
+
+
+
   render(){
     const { todos } = this.props;
+    const todoState = this.props.mappedTodoState;
+    // const todos = todoState.todos;
+    // const editTodo = todoState.todoToEdit;
     console.log(this.props)
     return (
         <div id="Todo">
@@ -27,7 +59,7 @@ class Todo extends React.Component {
           {this.props.todos.error && <span className="text-danger">ERROR: {this.props.todos.error}</span>}
           <div className="glass">
           <div className="glass-title">
-            <h3>My todo list:   <button type="button" className="btn btn-sm btn-default left table-btn add-btn">+</button></h3>
+            <h3>My todo list:   <button type="button" onClick={() => this.showEditModal()} className="btn btn-sm btn-default left table-btn add-btn">+</button></h3>
 
           </div>
           {this.props.todos.items && <Table striped bordered condensed hover className="todoT"><tbody>
